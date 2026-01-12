23dd1ed3ce80858f87b7a9f840b89f08a26a77e8
Anjana-NK <anjana83nk@gmail.com>
2026-01-12 12:51:28 +0530

    added exercise 2.2

---
+++ ex22.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main() {
+    int a, b, c;
+    float x;
+
+    /* (a) */
+    a = 250;
+    b = 85;
+    c = 25;
+    x = a / (b - c);
+    printf("(a): x = %f\n", x);
+
+    /* (b) */
+    a = 300;
+    b = 70;
+    c = 70;
+    x = a / (b - c);
+    printf("(b): x = %f\n", x);
+
+    
+}
