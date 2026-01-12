65f8202cbbafe0e1fd93df1254016d47d5cb3ea7
Anjana-NK <anjana83nk@gmail.com>
2026-01-12 11:48:42 +0530

    tutorials

---
+++ tutorial3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    printf("ANJANA NK\n25109987\ndept of computer science\tcusat\n");
+    int a,b,sum;
+    a=10;
+    b=20;
+    sum=a+b;
+    printf("sum is %d",sum);
+}
