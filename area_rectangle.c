987c4b4e354d8c281fbac1166cf5100d24ab188f
Anjana-NK <anjana83nk@gmail.com>
2026-01-14 20:02:34 +0530

    added area of rectangle prgm

---
+++ area_rectangle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    float l,b,area;
+    printf("enter length and breadth of rectangle");
+    scanf("%f%f",&l,&b);
+    area=l*b;
+    printf("area of rectangle is %f",area);
+}
