db695f8f6e17739dacdb14a3f93b9a0eeb800937
Anjana <anjana83nk@gmail.com>
2026-01-14 20:56:25 +0530

    Add perimeter calculation to rectangle area program

--- area_rectangle.c
+++ area_rectangle.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 void main(){
-    float l,b,area;
+    float l,b,p,area;
     printf("enter length and breadth of rectangle");
     scanf("%f%f",&l,&b);
     area=l*b;
+    p=2*(l+b);
     printf("area of rectangle is %f",area);
+    printf("perimeter of rectangle is %f",p);
 }
