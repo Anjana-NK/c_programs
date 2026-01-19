b8f43d16ca516c69481b1bc3c0ae70ff3de19a24
Anjana-NK <anjana83nk@gmail.com>
2026-01-19 20:06:40 +0530

    added swaping two variables

---
+++ swap.c
@@ -0,0 +1,13 @@
+// anjana nk dpt of cs
+#include<stdio.h>
+void main(){
+int a,b,t;
+printf("enter value of a");
+scanf("%d",&a);
+printf("\nenter value of b");
+scanf("%d",&b);
+a=b;
+b=t;
+t=a;
+printf("value of a is %d and of b is %d",a,b);
+}
