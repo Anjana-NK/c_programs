0ac81fa47919bc88a43873ddaf94a78d518e01e0
Anjana <anjana83nk@gmail.com>
2026-03-09 09:51:28 +0530

    Update labq4.c

--- fair_record/labq4.c
+++ fair_record/labq4.c
@@ -23,12 +23,9 @@ int main()
         }
     }
 
-    
-
-    // Step 1: assume first element is largest
+    // assume first element is largest
     largest = a[0][0];
-
-    // Step 2: find largest element in matrix
+    // find largest element in matrix
     for(i=0;i<m;i++)
     {
         for(j=0;j<n;j++)
@@ -37,11 +34,9 @@ int main()
                 largest = a[i][j];
         }
     }
-
-    // Step 3: assume second largest initially
+    // assume second largest initially
     second = a[0][0];
-
-    // Step 4: find largest element that is not equal to the largest
+    //find largest element that is not equal to the largest
     for(i=0;i<m;i++)
     {
         for(j=0;j<n;j++)
@@ -50,19 +45,14 @@ int main()
                 second = a[i][j];
         }
     }
-
     printf("Second largest element = %d\n",second);
-
-
-    
     // Check each row
     for(i=0;i<m;i++)
     {
         // Assume first element of row is minimum
         min = a[i][0];
         col = 0;
-
-        // Find smallest element in that row
+         // Find smallest element in that row
         for(j=1;j<n;j++)
         {
             if(a[i][j] < min)
@@ -71,14 +61,12 @@ int main()
                 col = j;        // store its column position
             }
         }
-
         // Now check that column to see if min is largest in column
         for(k=0;k<m;k++)
         {
             if(a[k][col] > min)
                 break;  // if any element is larger, not saddle point
         }
-
         // If loop completed fully, then it is saddle point
         if(k == m)
         {
@@ -86,7 +74,6 @@ int main()
             saddle = 1;
         }
     }
-
     // If no saddle point found
     if(saddle == 0)
         printf("No Saddle Point\n");
